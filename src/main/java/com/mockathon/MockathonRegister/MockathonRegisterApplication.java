14fb6f334c3211ea4155b1756a304f6982d7eeac
akshata <akshata.malwad@gmail.com>
2018-04-06 12:12:14 +0530

    commit 1

---
+++ src/main/java/com/mockathon/MockathonRegister/MockathonRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.mockathon.MockathonRegister;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockathonRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockathonRegisterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mockathon/MockathonRegister/MockathonRegisterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mockathon.MockathonRegister;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MockathonRegisterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
